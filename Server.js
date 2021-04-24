896b4eff750b87929c4c139c145cb7297a9be3dd
bokekez <bokekez@gmail.com>
2021-04-24 15:49:23 +0200

    2

--- Server.js
+++ Server.js
@@ -24,12 +24,64 @@ const db = knex({
     }
 });
 
-  app.get('/', (req, res) =>{
+app.get('/', (req, res) =>{
       db.select('id', 'item', 'price', 'sales').from('items')
       .then(data =>{
           res.json(data)
       })
-  })
+})
 
-  console.log('10 4 dinosaur')
-  app.listen(3000);
+app.post('/register', (req, res) => {
+	const email = req.body.email;
+	const username = req.body.username;
+	const password = req.body.password;
+	// const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			username: username,
+			email: email,
+      password: password
+		})
+		.into('users')
+    .returning('email')
+	// .returning('email')
+		// .then(loginEmail =>{
+    //   return trx('users')
+    //   .returning('*')
+    //       .insert({
+    //       email: loginEmail[0],
+    //       username: username,
+		// 	    })
+		.then(user => {
+				  res.json(user[0]);
+          console.log(user);
+			    })
+		// })
+	  .then(trx.commit)
+	  .catch(trx.rollback)
+	})
+  .catch(err => res.status(400).json('unable to register'));
+
+})
+
+app.post('/login', (req, res)=>{
+	db.select('email').from('users')
+	.where('email', '=', req.body.email)
+	.then(data => {
+		const isValid = req.body.password;
+		if (isValid){
+			return 	db.select('*').from('users')
+			.where('email', '=', req.body.email)
+			.then(user => {
+				console.log(user);
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('Unable to get user'))
+		} else {
+			res.status(400).json('Wrong credentials')
+		}
+	})
+})
+
+console.log('10 4 dinosaur')
+app.listen(3000);
