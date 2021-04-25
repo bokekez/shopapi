92fa3ab4faa1dbb1eac6c5937eb5f983876a6c4c
bokekez <bokekez@gmail.com>
2021-04-25 21:45:34 +0200

    2

--- Server.js
+++ Server.js
@@ -8,8 +8,12 @@ const app = express();
 
 app.use(cors())
 
+app.options('*', cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+
 
 const db = knex({
      client: 'pg',
