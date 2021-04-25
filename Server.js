135149706abc34aab7f1c185cf50cf38a1912bdb
bokekez <bokekez@gmail.com>
2021-04-26 00:06:36 +0200

    2

--- Server.js
+++ Server.js
@@ -11,14 +11,6 @@ app.use(cors());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
 
-app.all('/*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
-  });
-
-
-
 const db = knex({
      client: 'pg',
      connection: {
