e48b81ad4850f439dab0f7639b3ed4bd0c662df7
bokekez <bokekez@gmail.com>
2021-04-25 23:57:51 +0200

    2

--- Server.js
+++ Server.js
@@ -11,6 +11,11 @@ app.use(cors({credentials: true, origin: true})));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
 
+app.all('/*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+  });
 
 
 
