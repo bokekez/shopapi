122d9c8636c4a697ff1b44b2048b70682a244284
bokekez <bokekez@gmail.com>
2021-05-18 19:40:09 +0200

    picture editing fix

--- Server.js
+++ Server.js
@@ -111,21 +111,39 @@ app.post('/profile', cors(), (req, res) => {
 	const username = req.body.username;
 	const picture = req.body.picture;
 	db.transaction(trx => {
-		trx.insert({
-			item: item,
-			price: price,
-			username: username,
-			picture: picture
-		})
-		.into('items')
-    	.returning('item')
-		.then(item => {
-          console.log(item);
-				  res.json(item[0]);
-          console.log(item);
-			    })
-	  .then(trx.commit)
-	  .catch(trx.rollback)
+		if(picture !== null){
+			trx.insert({
+				item: item,
+				price: price,
+				username: username,
+				picture: picture
+			})
+			.into('items')
+			.returning('item')
+			.then(item => {
+			  console.log(item);
+					  res.json(item[0]);
+			  console.log(item);
+					})
+		  .then(trx.commit)
+		  .catch(trx.rollback)
+		}
+		if(picture === null){
+			trx.insert({
+				item: item,
+				price: price,
+				username: username,
+			})
+			.into('items')
+			.returning('item')
+			.then(item => {
+			  console.log(item);
+					  res.json(item[0]);
+			  console.log(item);
+					})
+		  .then(trx.commit)
+		  .catch(trx.rollback)
+		}
 	})
   .catch(err => res.status(400).json('unable to create'));
 
